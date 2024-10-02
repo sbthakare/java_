bdf4d5db577bc1525a3d4d397b8f2c352641ddd0
Swapnil Thakare <78209244+sbthakare@users.noreply.github.com>
2024-10-02 22:04:42 +0530

    Add files via upload

---
+++ assignment6/Q2/TestGeometric.java
@@ -0,0 +1,20 @@
+package assignment6;
+
+public class TestGeometric {
+
+	public static void main(String[] args) {
+		circle circle1=new circle(5);
+	     circle1.area();
+	     circle1.perimeter();
+	     
+	     Rectangle rectangle=new Rectangle(2, 3);
+	     rectangle.area();
+	     rectangle.perimeter();
+	     
+	     Triangle triangle=new Triangle(2,4);
+	     triangle.area();
+	     triangle.perimeter();
+
+	}
+
+}
