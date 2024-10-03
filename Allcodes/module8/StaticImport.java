3eadc8ffdd6a6aefbaacef10de3e9bf4a36acb14
Swapnil Thakare <78209244+sbthakare@users.noreply.github.com>
2024-10-03 18:01:42 +0530

    Add files via upload

---
+++ Allcodes/module8/StaticImport.java
@@ -0,0 +1,24 @@
+package module8;
+import static java.lang.Math.*;    // import package
+public class StaticImport {
+
+	public static void main(String[] args) {
+		System.out.println(pow(3,4));
+
+		System.out.println(abs(-39));
+		
+		System.out.println(floor(34.67));
+		System.out.println(ceil(34.67));
+		
+		System.out.println(min(30,40));
+		System.out.println(max(30,40));
+		
+		System.out.println(random());
+		
+		System.out.println(round(22.8));
+		
+		System.out.println(sqrt(64));
+		
+	}
+
+}
